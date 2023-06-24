8b826278816cc1c8c260afad188537d66c6c3cd3
CoderLtrotti <120693492+CoderLtrotti@users.noreply.github.com>
2023-06-24 20:54:23 -0300

    cambios

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -25,7 +25,26 @@ viewsRouter.get('/', (req , res) =>{
         title: 'Inicio de sesion',
     });   
  });
+ viewsRouter.get('/product-details', (req, res) =>{
+    res.render('product-details');
+});
 
+viewsRouter.post('/add-to-cart/:id', (req, res) => {
+    // Aquí obtendrías el id del producto desde la URL y la cantidad desde el cuerpo de la solicitud (req.body.quantity)
+    const productId = req.params.id;
+    const quantity = req.body.quantity;
+  
+    // Aquí podrías agregar el producto al carrito según el id y la cantidad
+  
+    res.redirect('/cart');
+  });
+
+viewsRouter.get('/cart-details', (req, res) =>{
+    res.render('cart-details');
+});
+  
+
+  
 
 
 export default viewsRouter;
